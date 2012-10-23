27d6ae4e9abd2bcd3d20edca5a28dcb1bd39c6aa
Issei Naruta <naruta@cookpad.com>
2012-10-23 13:47:50 +0900

    Separate files with gify directory

---
+++ lib/gify/thor_starter.rb
@@ -0,0 +1,48 @@
+require 'thor'
+require_relative 'gif'
+
+module Gify
+  class ThorStarter < Thor
+    include Thor::Actions
+
+    desc 'create', 'convert'
+    method_option :out,    type: :string,  aliases: '-o'
+    method_option :delay,  type: :numeric, default: 10
+    method_option :tumblr, type: :boolean, default: true
+    def create(*files)
+      opts = "-delay #{options[:delay]} -loop 0"
+      opts += ' -resize \>500' if options[:tumblr]
+      run %Q(convert #{opts} #{files.join(" ")} #{outgif})
+      say_status 'created', outgif
+      check_tumblr_friendliness(outgif)
+    end
+
+    desc 'check_tumblr_friendliness', 'check tumblr friendliness'
+    def check_tumblr_friendliness(gif)
+      gif = Gif.new gif
+      say_status 'meta', gif.meta
+      if gif.tumblr_friendly?
+        say_status 'tumblr?', 'good', :green
+      else
+        say_status 'tumblr?', 'bad', :red
+      end
+    end
+
+    no_tasks do
+      def random_name
+        chr = ('a'..'z').to_a
+        name = ''
+        5.times { name += chr.sample }
+        "gify-#{name}.gif"
+      end
+
+      def outgif
+        @outgif ||= begin
+          out = options[:out] || random_name
+          out += '.gif' unless out =~ /\.gif$/
+          Gif.new out
+        end
+      end
+    end
+  end
+end
---
+++ lib/gify/version.rb
@@ -0,0 +1,6 @@
+module Gify
+  module_function
+  def version
+    '0.0.1'
+  end
+end
